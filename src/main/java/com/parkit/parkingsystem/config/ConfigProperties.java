b83249d10537d43644fdad5a774ebbeb330e91e9
Dimitri Benjamin <dimbenjamin05@gmail.com>
2021-06-18 13:25:40 -0400

    Amélioration de code

--- src/main/java/com/parkit/parkingsystem/config/ConfigProperties.java
+++ src/main/java/com/parkit/parkingsystem/config/ConfigProperties.java
@@ -1,12 +1,10 @@
 package com.parkit.parkingsystem.config;
 
-//@Configuration
-//@ConfigurationProperties(prefix = "config")
+
 public class ConfigProperties {
 	  private String hostName; 
 	    private int port; 
 	    private String from; 
 
-	    // standard getters and setters 
 	
 }
