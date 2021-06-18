2b0d9d51d729940202109c721559cdcf64191c26
Dimitri Benjamin <dimbenjamin05@gmail.com>
2021-06-18 13:13:28 -0400

    Removed target  directory

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -1,9 +1,13 @@
 
+
+
 package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.service.InteractiveShell;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 public class App {
     private static final Logger logger = LogManager.getLogger("App");
